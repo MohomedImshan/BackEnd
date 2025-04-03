bf23754cd869f4c78f0cdf000f5d372b0cd0f88c
MohomedImshan <mohomedamccimshan@gmail.com>
2025-04-03 20:16:17 +0530

    #1 create the user table

---
+++ db/db.js
@@ -0,0 +1,16 @@
+import mysql from 'mysql'
+
+const db = mysql.createConnection({
+    host:'127.0.0.1',
+    user:'root',
+    password:'',
+    database:'spare-parts-management-system'
+})
+db.connect((err)=>{
+    if(err){
+        console.error("Database connection failed : "+err.message)
+    }else{
+        console.log("Connected to mysql database")
+    }
+})
+export default db
--- server.js
+++ server.js
@@ -1,20 +1,19 @@
 import express from "express"
 import bodyParser from "body-parser"
 import cors from "cors"
+import userConnection from './User/userConnection.js'
+import db from './db/db.js'
 
 
 const app = express()
-const port = 8800
+const port = 8801
 
 app.use(express.json())
 app.use(cors())
 app.use(bodyParser.json())
 
+app.use('/User',userConnection)
 
 
 
-
-
-
-
-app.listen(8800,()=>console.log(`Listen on ${port}`))
+app.listen(8801,()=>console.log(`Listen on ${port}`))

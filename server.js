bc3a08ee071df9fb07802a1a980bf363ca7bc2c8
damayanthi <your.email@example.com>
2025-06-25 22:08:57 +0530

    spareparts tablebackend

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ import userConnection from './User/userConnection.js'
 import EngineerConnection from './User/EngineerConnection.js'
 import registerRoutes from './routes/registerRoutes.js'
 import db from './db/db.js'
-
+import sparePartsRoutes from './routes/sparePartsRoutes.js';
 
 const app = express()
 const port = 8801
@@ -18,6 +18,6 @@ app.use('/User',userConnection)
 app.use('/Engineer',EngineerConnection)
 app.use('/api/register',registerRoutes)
 
-
+app.use('/api/spareparts', sparePartsRoutes);
 
 app.listen(8801,()=>console.log(`Listen on ${port}`))

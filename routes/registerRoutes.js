4cc3054b02978b9f5ffa7f4b4e7ba3e982266a5e
MohomedImshan <mohomedamccimshan@gmail.com>
2025-07-01 13:27:05 +0530

    back end for login and register.

--- routes/registerRoutes.js
+++ routes/registerRoutes.js
@@ -5,6 +5,8 @@ import db from '../db/db.js'
 const router = express.Router()
 
 router.post('/', async (req, res) => {
+
+    console.log("Request body received:",req.body);
     const { userName, email, password, position } = req.body;
 
     if (!userName || !email || !password || !position) {
@@ -12,21 +14,20 @@ router.post('/', async (req, res) => {
     }
 
     try {
-        const [existingUser] = await db.query("SELECT * FROM users WHERE email = ?", [email]);
+        const existingUser = await db.query("SELECT * FROM users WHERE email = ?", [email]);
 
         if (existingUser.length > 0) {
-            return res.status(409).json({ message: "Email is already registered" });
+            return res.json({ message: "Email is already registered" });
         }
 
         const hashedPassword = await bcrypt.hash(password, 10);
         const sql = "INSERT INTO users (userName, email, password, position) VALUES (?, ?, ?, ?)";
         await db.query(sql, [userName, email, hashedPassword, position]);
-
-        res.status(201).json({ message: "User registered successfully!" });
+        res.json({ message: "User registered successfully!" });
 
     } catch (err) {
         console.error("Registration error:", err);
-        res.status(500).json({ message: 'Server error' });
+        res.json({ message: 'Server error' });
     }
 });
 
--- server.js
+++ server.js
@@ -4,20 +4,22 @@ import cors from "cors"
 import userConnection from './User/userConnection.js'
 import EngineerConnection from './User/EngineerConnection.js'
 import registerRoutes from './routes/registerRoutes.js'
+import loginRoutes from './routes/loginRoutes.js'
 import db from './db/db.js'
 
 
 const app = express()
-const port = 8801
+const port = 8800
 
 app.use(express.json())
 app.use(cors())
-app.use(bodyParser.json())
+//app.use(bodyParser.json())
 
 app.use('/User',userConnection)
 app.use('/Engineer',EngineerConnection)
 app.use('/api/register',registerRoutes)
+app.use('/api/login',loginRoutes)
 
 
 
-app.listen(8801,()=>console.log(`Listen on ${port}`))
+app.listen(port,()=>console.log(`Listen on ${port}`))

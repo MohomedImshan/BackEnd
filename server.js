5abd5ab2e1798643d6db7b8a28178eb626ff52a9
Pabasara Samarakoon <pabasara96samarakoon@gmail.com>
2025-08-28 00:19:24 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 import express from "express";
 import cors from "cors";
+import bodyParser from "body-parser";
+
 
 import userConnection from './User/userConnection.js';
 import EngineerConnection from './User/EngineerConnection.js';

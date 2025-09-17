a509a978e81f33378777fbd44045e491b339d322
MohomedImshan <mohomedamccimshan@gmail.com>
2025-09-17 22:35:21 +0530

    view of my requests

--- routes/ownrequestsRoutes.js
+++ routes/ownrequestsRoutes.js
@@ -16,6 +16,30 @@ router.get('/:empNum', (req, res) => {
     });
     });
 
-
+    router.get("/:empNum/:id", (req, res) => {
+        const empNum = req.params.empNum;
+        const id = req.params.id
+      
+        db.query("SELECT * FROM requests WHERE empNum = ? AND id=?", [empNum,id], (err, results) => {
+          if (err) {
+            console.error("Fetch request error:", err);
+            return res.status(500).json({ error: "Database error", details: err.message });
+          }
+      
+          if (!results || results.length === 0) {
+            return res.status(404).json({ error: "Request not found" });
+          }
+      
+          const request = results[0];
+      
+          let spareParts=[]
+          try{
+            spareParts=request.parts?JSON.parse(request.parts):[]
+          }catch(parseErr){
+            console.error("Error parsing parts JSON:",parseErr)
+          }
+          res.json({...request,spareParts})
+        });
+      });
 
 export default router;

0cd0382584c97a1741a59927add017bcd128126d
MohomedImshan <152673385+MohomedImshan@users.noreply.github.com>
2025-11-03 01:47:22 +0530

    Update database connection to require all parameters
    Remove default values for database connection parameters.

--- db/db.js
+++ db/db.js
@@ -3,9 +3,9 @@ import dotenv from 'dotenv'
 
 const db = mysql.createConnection({
     host: process.env.DB_HOST,        
-    user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASS || '',
-    database: process.env.DB_NAME || 'spare-parts-management-system'
+    user: process.env.DB_USER ,
+    password: process.env.DB_PASS ,
+    database: process.env.DB_NAME
 })
 db.connect((err)=>{
     if(err){

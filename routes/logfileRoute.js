12c4b2dd25f3d48bd77f142e017f483a09ae157c
MohomedImshan <mohomedamccimshan@gmail.com>
2025-11-17 23:25:21 +0530

    log file changed

--- routes/logfileRoute.js
+++ routes/logfileRoute.js
@@ -1,17 +1,34 @@
 import express from 'express'
 import db from '../db/db.js'
 import verifyToken from './authentication.js'
+
 const router = express.Router()
 
-router.get('/',verifyToken, async (req, res) => {
-    const sql = "SELECT * FROM user_logs ORDER BY id DESC ";
-    db.query(sql, (err, data) => {
-        if (err) return res.status(500).json({ error: err.message });
-        return res.json({log: data});
-    });
-})
-router.get('/protected',verifyToken,(req,res)=>{
-    res.json({message:"You are authorized",user:req.user})
-})
+router.get('/', verifyToken, (req, res) => {
+    const { from, to } = req.query;
 
-export default router
+    let sql = "SELECT * FROM user_logs";
+    let params = [];
+
+    if (from && to) {
+        sql += " WHERE timestamp BETWEEN ? AND ?";
+        params.push(from + " 00:00:00", to + " 23:59:59");
+    } 
+    else if (from) {
+        sql += " WHERE timestamp >= ?";
+        params.push(from + " 00:00:00");
+    }
+    else if (to) {
+        sql += " WHERE timestamp <= ?";
+        params.push(to + " 23:59:59");
+    }
+
+    sql += " ORDER BY id DESC";
+
+    db.query(sql, params, (err, data) => {
+        if (err) return res.status(500).json({ error: err.message });
+        return res.json({ log: data });
+    });
+});
+
+export default router 

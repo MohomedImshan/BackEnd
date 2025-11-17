c5e70f8b931a5846e7429ffb34e81a86476ac5f9
MohomedImshan <mohomedamccimshan@gmail.com>
2025-11-18 00:50:39 +0530

    changed related to server

--- db/db.js
+++ db/db.js
@@ -4,10 +4,10 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const db = mysql.createConnection({
-  host: process.env.DB_HOST||localhost,
-  user: process.env.DB_USER||root,
-  password: process.env.DB_PASS||'',
-  database: process.env.DB_NAME||spare-parts-management-system
+  host: process.env.DB_HOST||'localhost',
+  user: process.env.DB_USER||'root',
+  password: process.env.DB_PASSWORD||'',
+  database: process.env.DB_NAME||'spare-parts-management-system'
 });
 
 db.connect((err) => {
